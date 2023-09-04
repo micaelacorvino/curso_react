ceb138a5941bab90260764e0c026ec4bc76dbfe4
micaelacorvino <micaela.corvino1@gmail.com>
2023-09-04 19:31:18 -0300

    Actualizacion segundapreentrega

--- src/asyncMock.js
+++ src/asyncMock.js
@@ -6,8 +6,8 @@ const products = [
       id: '1',
       name: 'Iphone 14',
       price: 300000,
-      category: 'celular',
-      img: 'https://images.app.goo.gl/bNkjbr5ZWdegP3XX8',
+      category: 'Celular',
+      img: 'https://d2ihpvt6nd5q28.cloudfront.net/wp-content/uploads/2023/02/iPhone_14_Pro_Silver_PDP_Image_Position-1a_MXLA.jpg',
       stock: 25,
       description: 'Descripción del Iphone 14',
     },
@@ -15,8 +15,8 @@ const products = [
       id: '2',
       name: 'Samsung S21',
       price: 250000,
-      category: 'celular',
-      img: 'https://www.rosarioalcosto.com/product-samsung-sm-g991bzwklaro-2103171554296904.h',
+      category: 'Celular',
+      img: 'https://coppelar.vtexassets.com/arquivos/ids/1712832/198_62940b0d2f10c.jpg?v=637895504152230000',
       stock: 25,
       description: 'Descripción del Samsung S21',
     },
@@ -24,8 +24,8 @@ const products = [
       id: '3',
       name: 'Ipad 8va generación',
       price: 300000,
-      category: 'tablet',
-      img: 'https://images.app.goo.gl/4mw8grJbVLhtCW4b9',
+      category: 'Tablet',
+      img: 'https://www.cqnetcr.com/109101-thickbox_default/tablet-apple-ipad-102-wi-fi-8-generacion-3-gb.jpg',
       stock: 25,
       description: 'Descripción del Ipad 8va generación',
     },
@@ -49,14 +49,14 @@ const products = [
     });
   };
 
-  export const getProductsByCategory = (categoryId) => {
+  export const getProductsByCategory = (category) => {
     return new Promise((resolve) => {
       setTimeout(() => {
-        const filteredProducts = products.filter(prod => prod.category === categoryId);
+        
+        const filteredProducts = products.filter((product) => product.category === category);
         resolve(filteredProducts);
       }, 500);
     });
   };
-
   
  
